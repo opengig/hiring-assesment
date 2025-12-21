f851afb894f10ead0c8f4723dfd3e5102b57d29f
Nilesh <nilesh.28.2003@gmail.com>
2025-12-21 20:28:42 +0530

    fix: remove all unnecessary files

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,16 +2,13 @@
 import { ThemeProvider } from '@/components/theme-providers';
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { SessionProvider } from 'next-auth/react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<SessionProvider>
-			<ThemeProvider attribute='class' defaultTheme='system' disableTransitionOnChange>
-				<Toaster duration={2500} richColors closeButton position='top-right' />
-				<TooltipProvider>{children}</TooltipProvider>
-			</ThemeProvider>
-		</SessionProvider>
+		<ThemeProvider attribute='class' defaultTheme='system' disableTransitionOnChange>
+			<Toaster duration={2500} richColors closeButton position='top-right' />
+			<TooltipProvider>{children}</TooltipProvider>
+		</ThemeProvider>
 	);
 };
 
